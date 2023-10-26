87e7979b22ceb8eaa592a6550b0f4a21f1b7e1c7
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2023-10-26 07:51:11 -0300

    docs: comentários

--- generics_exemplo/src/control/GenericFunctionController.java
+++ generics_exemplo/src/control/GenericFunctionController.java
@@ -1,17 +1,15 @@
 package control;
 
-public class GenericaFunctionController {
+public class GenericFunctionController {
 	public static void main(String[] args) {
 		System.out.printf("Maximum of %d, %d and %d is %d%n%n", 3, 4, 5, maximum(3, 4, 5));
 		System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f%n%n", 6.6, 8.8, 7.7, maximum(6.6, 8.8, 7.7));
-		System.out.printf("Maximum of %s, %s and %s is %s%n", "pear", "apple", "orange",
-				maximum("pear", "apple", "orange"));
+		System.out.printf("Maximum of %s, %s and %s is %s%n", "pear", "apple", "orange", maximum("pear", "apple", "orange"));
 	}
 
 	/* 
 	 * Nota: O compilador vai fazer a operação de Erasure. Isto é traduz o tipo genérico para substituir os tipos reais.
 	 * */
-	// determines the largest of three Comparable objects
 	public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
 		T max = x; // assume x is initially the largest
 
