d1f13e5fa66d845821c534929ac9feb83d8c3137
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-07-31 13:17:18 -0300

    docs: exception

--- objetivo4_tratamento_excecoes_v/src/dao/BaseDAO.java
+++ objetivo4_tratamento_excecoes_v/src/dao/BaseDAO.java
@@ -1,7 +1,5 @@
 package dao;
 
-import jdk.jshell.spi.ExecutionControl;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,21 +10,12 @@ public class BaseDAO {
 		final String url = "jdbc:mariadb://localhost:3306/lpoo_vendas_jdbc";
 
 		try {
+			//argumentos: url para o banco, usuário, senha
+			//retorna um objeto da classe Connection (do pacote java.sql -> que segue a especificação JDBC).
 			return DriverManager.getConnection(url, "mariadb", "mariadb");
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
-		//argumentos: url para o banco, usuário, senha.
-		//retorna um objeto da classe Connection (do pacote java.sql -> que segue a especificação JDBC).
-
-//		try {
-//			//a string com a url para o banco de dados
-//			//sintaxe: protocolo:tecnologia://domínioDoServidor:porta/database
-//
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			return null; //se não conectar, retorna null.
-//		}
 	}
 	
 	//um main para testar a conexão com o servidor do MariaDB
